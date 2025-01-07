771d5f311c0d1098785a9a65ecf90227270b1559
Ziad Ahmed <zaid.ahmed@quickcheck.ng>
2025-01-08 00:34:55 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, {Request, Response, NextFunction} from "express";
+import dotenv from "dotenv";
+import router from './routes/index';
+import connectDB from './config/db.config'
+import cors from "cors";
+
+dotenv.config()
+
+const PORT: number = +(process.env.PORT || "5000");
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+
+connectDB();
+
+app.use("/api", router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Server Error' });
+});
+
+export default app;
---
+++ src/config/db.config.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const MONGO_URI: string = process.env.MONGO_URI || "mongodb://localhost:27017";
+
+const connectDB = async () => {
+    mongoose.connect(MONGO_URI, {}).then(() => {
+        console.log("Connected to DB");
+    }).catch(err => {
+        console.error("Couldn't connect to db due: ", err.message);
+        process.exit(1);
+    });
+
+};
+
+export default connectDB;
